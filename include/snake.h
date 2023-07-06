86f61011c41c81ecc4cbb68c5e29c22d85e98606
d-chandrahas <chandrahasreddy732@gmail.com>
2023-07-06 23:28:28 +0530

    just a backup, in progress

--- include/snake.h
+++ include/snake.h
@@ -1,21 +1,46 @@
 #ifndef SNAKE_H
 #define SNAKE_H
 
-
 typedef unsigned short int usint;
 
 typedef struct Node Node;
 
 typedef struct Snake Snake;
 
+Node *Node_Allocate(void);
 
+Node *Node_Construct(Node *node_p, usint y, usint x, char heading, Node *prev_p, Node *next_p);
 
-void Node_construct (Node *node_p, usint y, usint x, char dir, Node *prev_p, Node *next_p);
+Node *New_Node(usint y, usint x, char heading, Node *prev_p, Node *next_p);
 
-void Snake_construct (Snake *snake_p);
+usint Node_GetFollowerY(const Node *followedNode_p, char relPos);
 
-void Snake_append (Snake *snake_p, usint y, usint x, char dir);
+usint Node_GetFollowerX(const Node *followedNode_p, char relPos);
 
+char Node_FollowerHeadingToRelPos(const Node *followedNode_p, char heading);
 
+Snake *Snake_Allocate(void);
+
+Snake *Snake_Construct(Snake *snake_p);
+
+Snake *New_Snake(void);
+
+usint Snake_GetLength(const Snake *snake_p);
+
+Node *Snake_GetHeadPtr(const Snake *snake_p);
+
+Node *Snake_GetTailPtr(const Snake *snake_p);
+
+usint Snake_GetTailY(const Snake *snake_p);
+
+usint Snake_GetTailX(const Snake *snake_p);
+
+char Snake_GetTailHeading(const Snake *snake_p);
+
+usint Snake_GetAppendX(Snake *snake_p);
+
+usint Snake_GetAppendY(Snake *snake_p);
+
+Snake *Snake_Append(Snake *snake_p);
 
 #endif
--- src/snake.c
+++ src/snake.c
@@ -1,56 +1,187 @@
-#include <stddef.h>
+#include <stdlib.h>
 #include "snake.h"
 
-
-struct Node {
+struct Node
+{
 	usint y;
 	usint x;
-	char dir;
+	char heading;
 	Node *prev_p;
 	Node *next_p;
 };
 
-struct Snake {
+struct Snake
+{
 	usint len;
 	Node *head_p;
 	Node *tail_p;
 };
 
-
-void Node_construct (Node *node_p, usint y, usint x, char dir, Node *prev_p, Node *next_p) {
-	node_p->y = y;
-	node_p->x = x;
-	node_p->dir = dir;
-	node_p->prev_p = prev_p;
-	node_p->next_p = next_p;
-	return;
+Node *Node_Allocate(void)
+{
+	return (Node *)malloc(sizeof(Node));
 }
 
-void Snake_construct (Snake *snake_p) {
+Node *Node_Construct(Node *node_p, usint y, usint x, char heading, Node *prev_p, Node *next_p)
+{
+	node_p->y = y;
+	node_p->x = x;
+	node_p->heading = heading;
+	node_p->prev_p = prev_p;
+	node_p->next_p = next_p;
+	return node_p;
+}
+
+Node *New_Node(usint y, usint x, char heading, Node *prev_p, Node *next_p)
+{
+	return Node_Construct(Node_Allocate(), y, x, heading, prev_p, next_p);
+}
+
+usint Node_GetFollowerY(const Node *followedNode_p, char relPos)
+{
+	if (relPos == 'u')
+	{
+		return followedNode_p->y - 1;
+	}
+	else if (relPos == 'd')
+	{
+		return followedNode_p->y + 1;
+	}
+	else if (relPos == 'l' || relPos == 'r')
+	{
+		return followedNode_p->y;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+usint Node_GetFollowerX(const Node *followedNode_p, char relPos)
+{
+	if (relPos == 'l')
+	{
+		return followedNode_p->x - 1;
+	}
+	else if (relPos == 'r')
+	{
+		return followedNode_p->x + 1;
+	}
+	else if (relPos == 'u' || relPos == 'd')
+	{
+		return followedNode_p->x;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+char Node_FollowerHeadingToRelPos(const Node *followedNode_p, char heading)
+{
+	if (heading == 'u')
+	{
+		return 'd';
+	}
+	else if (heading == 'd')
+	{
+		return 'u';
+	}
+	else if (heading == 'l')
+	{
+		return 'r';
+	}
+	else if (heading == 'r')
+	{
+		return 'l';
+	}
+	else
+	{
+		return '\0';
+	}
+}
+
+Snake *Snake_Allocate(void)
+{
+	return (Snake *)malloc(sizeof(Snake));
+}
+
+Snake *Snake_Construct(Snake *snake_p)
+{
 	snake_p->len = 0;
 	snake_p->head_p = NULL;
 	snake_p->tail_p = NULL;
-	return;
+	return snake_p;
 }
 
-void Snake_append (Snake *snake_p, usint y, usint x, char dir) {
-	Node *temp_node_p = (Node *) malloc (sizeof(Node));
-	if (snake_p->len == 0) {
-		Node_construct (temp_node_p, y, x, dir, NULL, NULL);
-		snake_p->head_p = temp_node_p;
+Snake *New_Snake(void)
+{
+	return Snake_Construct(Snake_Allocate());
+}
+
+usint Snake_GetLength(const Snake *snake_p)
+{
+	return snake_p->len;
+}
+
+Node *Snake_GetHeadPtr(const Snake *snake_p)
+{
+	return snake_p->head_p;
+}
+
+Node *Snake_GetTailPtr(const Snake *snake_p)
+{
+	return snake_p->tail_p;
+}
+
+usint Snake_GetTailY(const Snake *snake_p)
+{
+	return Snake_GetTailPtr(snake_p)->y;
+}
+
+usint Snake_GetTailX(const Snake *snake_p)
+{
+	return Snake_GetTailPtr(snake_p)->x;
+}
+
+char Snake_GetTailHeading(const Snake *snake_p)
+{
+	return Snake_GetTailPtr(snake_p)->heading;
+}
+
+usint Snake_GetAppendX(Snake *snake_p)
+{
+	return Node_GetFollowerX(
+		Snake_GetTailPtr(snake_p),
+		Node_FollowerHeadingToRelPos(
+			Snake_GetTailPtr(snake_p),
+			Snake_GetTailHeading(snake_p)));
+}
+
+usint Snake_GetAppendY(Snake *snake_p)
+{
+	return Node_GetFollowerY(
+		Snake_GetTailPtr(snake_p),
+		Node_FollowerHeadingToRelPos(
+			Snake_GetTailPtr(snake_p),
+			Snake_GetTailHeading(snake_p)));
+}
+
+Snake *Snake_Append(Snake *snake_p)
+{
+	if (Snake_Length(snake_p) == 0)
+	{
+		return NULL;
 	}
-	else {
-		Node_construct (temp_node_p, y, x, dir, snake_p->tail_p, NULL);
-		snake_p->tail_p->next_p = temp_node_p;
-	}
-	snake_p->tail_p = temp_node_p;
+
+	Node *newNode_p = New_Node(
+		Snake_GetAppendY(snake_p),
+		Snake_GetAppendX(snake_p),
+		Snake_GetTailHeading(snake_p),
+		Snake_GetTailPtr(snake_p),
+		NULL);
+	snake_p->tail_p->next_p = newNode_p;
+	snake_p->tail_p = newNode_p;
 	(snake_p->len)++;
-	return;
+	return snake_p;
 }
-
-
-	
-
-
-
-
